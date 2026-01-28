dc5ddb4dfc48735be74bbd1b8cc74eaeb5cf42dc
Link4Electronics <link4electronics@gmail.com>
2026-01-27 22:57:07 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/xash-material.png
 export DESKTOP=/usr/share/applications/xash3d.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xash3d
